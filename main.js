94055643061f16495bc67ae436330a848a84b115
tamer-alhashmi <tam.elhashmy@gmail.com>
2023-02-22 23:46:52 +0200

    a

--- main.js
+++ main.js
@@ -2994,43 +2994,6 @@ Info
 
 // let lis = document.querySelectorAll("ul li");
 // let exp = document.querySelector(".experiment");
-<<<<<<< HEAD
-
-// //First you must be sure if the color value already in Local Storage
-// // window.localStorage.clear();
-// if (window.localStorage.getItem("color")){
-//   // [1] Add color to div
-//   exp.style.backgroundColor = window.localStorage.getItem("color");
-
-//   // [2] Remove activ class from another lis
-//   lis.forEach((li) => {
-//     li.classList.remove("active");
-
-//   // [3] Add active class to current color
-//   document.querySelector(
-//     `[data-color="${window.localStorage.getItem("color")}"]`
-//   ).classList.add("active");
-//   });
-// }else{
-//   console.log("No");
-// }
-
-// lis .forEach((li) =>{
-//   li.addEventListener("click", (e) =>{
-//     // console.log(e.currentTarget.dataset.color);
-//     //remove active class from all lis
-//     lis.forEach((li) =>{
-//       li.classList.remove("active");
-//     });
-//     // Add Active Class to current element
-//     e.currentTarget.classList.add("active");
-//     // Add current color to Local Storage
-//     window.localStorage.setItem("color", e.currentTarget.dataset.color);
-//     // Change div background color
-//     exp.style.backgroundColor = e.currentTarget.dataset.color;
-//   })
-// })
-=======
 
 // //First you must be sure if the color value already in Local Storage
 // // window.localStorage.clear();
@@ -3305,4 +3268,3 @@ Sestructuring Array => Swapping Variables
 //   console.log("PLZ Select in our range")
 // };
 
->>>>>>> 04c60462edfeedf2382b75fb4ebbe5849f1f41ef
