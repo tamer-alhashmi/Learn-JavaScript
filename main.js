c6be59e0337fb08351b1eadf5ac5cd3e480660f4
tamer-alhashmi <tam.elhashmy@gmail.com>
2023-02-22 23:44:43 +0200

    #112

--- main.js
+++ main.js
@@ -2992,40 +2992,40 @@ Info
 
 // <!-- 112 Local Storage Color Application Practice  -->
 
-let lis = document.querySelectorAll("ul li");
-let exp = document.querySelector(".experiment");
+// let lis = document.querySelectorAll("ul li");
+// let exp = document.querySelector(".experiment");
 
-//First you must be sure if the color value already in Local Storage
-// window.localStorage.clear();
-if (window.localStorage.getItem("color")){
-  // [1] Add color to div
-  exp.style.backgroundColor = window.localStorage.getItem("color");
+// //First you must be sure if the color value already in Local Storage
+// // window.localStorage.clear();
+// if (window.localStorage.getItem("color")){
+//   // [1] Add color to div
+//   exp.style.backgroundColor = window.localStorage.getItem("color");
 
-  // [2] Remove activ class from another lis
-  lis.forEach((li) => {
-    li.classList.remove("active");
+//   // [2] Remove activ class from another lis
+//   lis.forEach((li) => {
+//     li.classList.remove("active");
 
-  // [3] Add active class to current color
-  document.querySelector(
-    `[data-color="${window.localStorage.getItem("color")}"]`
-  ).classList.add("active");
-  });
-}else{
-  console.log("No");
-}
+//   // [3] Add active class to current color
+//   document.querySelector(
+//     `[data-color="${window.localStorage.getItem("color")}"]`
+//   ).classList.add("active");
+//   });
+// }else{
+//   console.log("No");
+// }
 
-lis .forEach((li) =>{
-  li.addEventListener("click", (e) =>{
-    // console.log(e.currentTarget.dataset.color);
-    //remove active class from all lis
-    lis.forEach((li) =>{
-      li.classList.remove("active");
-    });
-    // Add Active Class to current element
-    e.currentTarget.classList.add("active");
-    // Add current color to Local Storage
-    window.localStorage.setItem("color", e.currentTarget.dataset.color);
-    // Change div background color
-    exp.style.backgroundColor = e.currentTarget.dataset.color;
-  })
-})
+// lis .forEach((li) =>{
+//   li.addEventListener("click", (e) =>{
+//     // console.log(e.currentTarget.dataset.color);
+//     //remove active class from all lis
+//     lis.forEach((li) =>{
+//       li.classList.remove("active");
+//     });
+//     // Add Active Class to current element
+//     e.currentTarget.classList.add("active");
+//     // Add current color to Local Storage
+//     window.localStorage.setItem("color", e.currentTarget.dataset.color);
+//     // Change div background color
+//     exp.style.backgroundColor = e.currentTarget.dataset.color;
+//   })
+// })
